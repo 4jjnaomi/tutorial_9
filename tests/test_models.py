224d0e2527b841fd14c52b47ea6a5c9b616520bd
4jjnaomi <115657585+4jjnaomi@users.noreply.github.com>
2024-01-18 23:21:20 +0000

    Added tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -24,3 +24,31 @@ def test_invalid_code_raises_error():
     """
     with pytest.raises(ValueError):
         Region("China", "China")
+
+
+def test_create_admin_valid():
+    """
+    GIVEN valid values an Admin object, email='test@test.com' and password='testpassword'
+    WHEN the Admin object 'admin' is created
+    THEN admin.email should equal 'test@test.com' and admin._password should be longer than 'testpassword' as it is a hashed value
+    """
+    admin = Admin("test@test.com", "testpassword")
+    assert admin.email == "test@test.com"
+    assert len(admin._password) > len("testpassword")
+
+def test_inavlid_event_type_raises_error():
+    """
+    GIVEN invalid data for the Event object event_type attribute, event_type="autumn"
+    WHEN the Event object is created
+    THEN a ValueException is raised
+    """
+    with pytest.raises(ValueError):
+        Event(event_type="autumn",
+                year=2032,
+                country="South Africa",
+                host="Cape Town",
+                noc="RSA",
+                start="01/01/2032",
+                end="14/01/2032",)
+        
+        
